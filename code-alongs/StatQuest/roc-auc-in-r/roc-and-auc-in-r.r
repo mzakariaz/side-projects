3f2a711cc3167560c3e6eb89520c45043798f3dd
mzakariaz <125213150+mzakariaz@users.noreply.github.com>
2024-06-25 10:14:10 +0100

    Saved plot images

--- code-alongs/StatQuest/roc-auc-in-r/roc-and-auc-in-r.r
+++ code-alongs/StatQuest/roc-auc-in-r/roc-and-auc-in-r.r
@@ -5,6 +5,10 @@
 # Install packages
 library(pROC)
 library(randomForest)
+library(rstudioapi)
+
+# Get the file path of the current script:
+current_directory_name <- dirname(getSourceEditorContext()$path)
 
 # Set random seed value
 set.seed(42)
@@ -24,6 +28,13 @@ obese <- ifelse(test = (random < rank(weight) / num_samples), yes = 1, no = 0)
 # Print the obese classification
 print(obese)
 
+# Set the working directory to the current directory
+setwd(normalizePath(getwd()))
+
+
+# Use as much space as possible for the plots
+par(pty = "m")
+
 # Plot the obese classification by weight
 plot(x = weight, y = obese)
 
@@ -36,11 +47,21 @@ predicted_probs <- predict(glm_fit, type = "response")
 # Draw the logistic regression curve associated to the predicted probabilities
 lines(weight, predicted_probs)
 
+# Save the plot as a .png image
+file_name_1 <- "/logistic_regression.png"
+file_path_1 <- paste(
+  current_directory_name,
+  file_name_1,
+  sep = ""
+)
+dev.copy(png, file = file_path_1)
+dev.off()
+
 # Initialise a random forest model
 rf_model <- randomForest(factor(obese) ~ weight)
 
-# Remove any padding in the plots
-par(pty = "s")
+# Use as much space as possible for the plot
+par(pty = "m")
 
 # Build and plot the ROC for the logistic regression
 roc_info <- roc(
@@ -83,8 +104,15 @@ legend(
   lwd = 4
 )
 
-# Use as much space as possible for each of the plots
-par(pty = "s")
+# Save the plot as a .png image
+file_name_2 <- "/roc-auc-logistic-regression-random-forest.png"
+file_path_2 <- paste(
+  current_directory_name,
+  file_name_2,
+  sep = ""
+)
+dev.copy(png, file = file_path_2)
+dev.off()
 
 # Tabulate the sensitivities and specificities of the logistic regression's ROC
 roc_df <- data.frame(
